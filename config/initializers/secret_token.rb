e270f73cb0376a10451c128df190a2082d6ced70
gatosaurio <hermeskybalion@hotmail.com>
2013-04-05 21:23:20 -0300

    Create user resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ribit::Application.config.secret_token = 'f75e67b638cd961222be1b3d3a4f3476476a0ead2c1c5a68ce8663fd47133967e2fcbfb1d5af129f7ea6f10f6c40b3eb8d4b206f2146cf1c34c2f297eaf89570'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ribit::Application.config.session_store :cookie_store, key: '_ribit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ribit::Application.config.session_store :active_record_store
