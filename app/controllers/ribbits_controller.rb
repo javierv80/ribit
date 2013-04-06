91d2846728e4207edb644269794a3dfaa965020d
gatosaurio <hermeskybalion@hotmail.com>
2013-04-06 03:14:34 -0300

    added the public ribbits page

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,11 +1,17 @@
 class RibbitsController < ApplicationController
+
+	def index
+		@ribbits = Ribbit.all :include => :user
+		@ribbit = Ribbit.new
+	end
+
 	def create
 		@ribbit = Ribbit.new(params[:ribbit])
 		@ribbit.user_id = current_user.id
 
 		if 
 			@ribbit.save
-			redirect_to current_user
+			redirect_to ribbits_path
 		else
 			flash[:error] = "Ooops!"
 			redirect_to current_user
